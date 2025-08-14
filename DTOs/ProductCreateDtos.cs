1104a8d6c278f49fbd36ac33eb4064ea432060f0
doug_rs1986 <dougemandy@gmail.com>
2025-08-14 00:30:05 -0300

    Separação das classes dos DTOs

--- DTOs/ProductCreateDtos.cs
+++ DTOs/ProductCreateDtos.cs
@@ -6,11 +6,5 @@ namespace artius_CadastroProdutos_WebAPI.DTOs
         public decimal Preco { get; set; }
         public string Categoria { get; set; }
     }
-    public class ProductReadDto
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public decimal Preco { get; set; }
-        public string Categoria { get; set; }
-    }
+
 }
---
+++ DTOs/ProductReadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace artius_CadastroProdutos_WebAPI.DTOs
+{
+    public class ProductReadDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
